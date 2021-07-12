972497062a1cb6e6b5def85f22ecc362f203b8d0
ramkumarthirugnanam <85556252+ramkumarthirugnanam@users.noreply.github.com>
2021-07-12 20:12:12 +0530

    Add files via upload

---
+++ server/models/Employee.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define collection and schema
+let Employee = new Schema({
+   name: {
+      type: String
+   },
+   address: {
+      type: String
+   },
+
+   phoneNumber: {
+      type: Number
+   },
+  state: {
+    type: String
+  },
+  city : {
+    type: String
+  }
+}, {
+   collection: 'employees'
+})
+
+module.exports = mongoose.model('Employee', Employee)
